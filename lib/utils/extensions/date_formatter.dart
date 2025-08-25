b075c5f7a9b331d8ec44b73925aa9facdfc06a8a
NUGROHOADI11 <deeforce112002@gmail.com>
2025-08-25 18:56:41 +0700

    change the logo

--- lib/utils/extensions/date_formatter.dart
+++
@@ -1,7 +0,0 @@
-import 'package:intl/intl.dart';
-
-extension DateFormatter on DateTime {
-  String toFormattedString({String format = 'dd MMM yyyy'}) {
-    return DateFormat(format).format(this);
-  }
-}
